275f54217156ad792c7317a5751f05f53c43a1de
ObSp <150492054+ObSp@users.noreply.github.com>
2024-07-19 14:05:03 +0200

    Added FlameTrainer

--- code/modules/stylesFixer.js
+++ code/modules/stylesFixer.js
@@ -1,4 +1,16 @@
 export function fixStyles(){
     const lastElementOfPastWork = document.getElementById("past-work").lastElementChild
     lastElementOfPastWork.style.marginBottom = getComputedStyle(lastElementOfPastWork).marginTop;
+
+    const usrNameDisp = document.getElementById("name-text")
+    const nameBounds = usrNameDisp.getBoundingClientRect()
+
+    const nameWidth = nameBounds.width + nameBounds.x
+    const viewportWidth = window.innerWidth
+
+    
+
+    if (nameWidth > viewportWidth){
+        
+    }
 }
